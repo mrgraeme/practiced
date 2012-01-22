13c27e854b4ff252e2cfbcd12f07db34446d7a4d
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2012-01-22 19:41:05 +0000

    Finished static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "should have the correct title" do
+      get 'home'
+      response.should have_selector("title", 
+                                    :content => "Practiced | Home")
     end
   end
 
@@ -13,6 +19,24 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+    it "should have the correct title" do
+      get 'contact'
+      response.should have_selector("title", 
+                                    :content => "Practiced | Contact")
+    end
+    
+  end
+  
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the correct title" do
+      get 'about'
+      response.should have_selector("title", 
+                                    :content => "Practiced | About")
     end
   end
 
