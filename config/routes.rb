a4780d38fb804f77589153632795e7a3d64fe498
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2012-03-10 13:00:04 +0000

    finished links and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Practiced::Application.routes.draw do
   
+  get "users/new"
+  
+  root :to => "pages#home"
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  
+  match '/signup', :to => 'users#new'
   
   
 
@@ -54,9 +60,7 @@ Practiced::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => "pages#home"
+ 
 
   # See how all your routes lay out with "rake routes"
 
