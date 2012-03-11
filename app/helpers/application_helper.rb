75ab87143780ec4c25b6fbb94ce3b581f1723733
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2012-03-11 12:25:53 +0000

    logo helper & integration test for links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,9 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+  
+  def logo
+    logo = image_tag("logo.png", :alt => "Practiced", :class => "round")
+  end
+  
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -11,19 +11,55 @@ describe "LayoutLinks" do
     get '/contact'
     response.should have_selector('title', :content => 'Contact')
   end
-  
+
   it "should have a about page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => 'About')
   end
-  
+
   it "should have a help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => 'Help')
   end
-  
+
   it "should have a signup page at '/signup'" do
     get "/signup"
     response.should have_selector('title', :content => 'Sign up')
   end
-end
+  
+  
+  
+
+  it "should have About page when clicking on the about link" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => 'About')
+  end
+  
+  it "should have Contact page when clicking on the about link" do
+    visit root_path
+    click_link "Contact"
+    response.should have_selector('title', :content => 'Contact')
+  end
+  
+  it "should have Help page when clicking on the about link" do
+    visit root_path
+    click_link "Help"
+    response.should have_selector('title', :content => 'Help')
+  end
+  
+  it "should have Home page when clicking on the about link" do
+    visit root_path
+    click_link "Home"
+    response.should have_selector('title', :content => 'Home')
+  end
+  
+  it "should have Signup page when clicking on the about link" do
+    visit root_path
+    click_link "sign up now!"
+    response.should have_selector('title', :content => 'Sign up')
+  end
+  
+
+  end
+
