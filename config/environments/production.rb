d3df495d31e30f9fde5dc258f979223e85874a09
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2012-04-21 12:43:04 +0100

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,8 @@ Practiced::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+  # and use secure cookies.
+  config.force_ssl = true
 end
