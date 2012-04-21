ed73d8debe13195020f925658404914e83f64486
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2012-04-21 12:41:03 +0100

    Finished User Signup - must fix factory testing..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,24 @@ class UsersController < ApplicationController
  
   def new
     @title = "Sign up"
+    @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Practiced!"
+      redirect_to @user
+    else
+      flash[:failure] = "There seems to have been a hiccup!"
+          @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
